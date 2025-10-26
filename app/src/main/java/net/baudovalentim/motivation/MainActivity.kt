0b28d9ba19763c03b81ee9e95c2f38cfd8310809
Fábio Valentim <maitabom@gmail.com>
2025-10-26 17:44:22 -0300

    feat: Adiciona layout inicial e assets
    Adiciona o layout inicial da `MainActivity`, incluindo um menu de categorias de frases, o nome do usuário, o local da frase e um botão para gerar uma nova frase.
    
    - Adiciona ícones vetoriais: `ic_happy`, `ic_all` e `ic_sunny`.
    - Define a cor `purple` nos recursos.
    - Configura o View Binding e o listener de clique para o botão "Nova Frase" na `MainActivity`.
    - Configura o mapeamento do VCS para Git.

--- app/src/main/java/net/baudovalentim/motivation/MainActivity.kt
+++ app/src/main/java/net/baudovalentim/motivation/MainActivity.kt
@@ -1,20 +1,46 @@
 package net.baudovalentim.motivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import net.baudovalentim.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setListeners()
     }
+
+    override fun onClick(v: View?) {
+      if (v.id == R.id.button_new_phrase) {
+          handleNewPhrase()
+      }
+    }
+
+    private fun handleNewPhrase() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setListeners() {
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+
 }
