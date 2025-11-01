acc8320d5c6332c7f2ccbe89c8b6fa529a327802
Fábio Valentim <maitabom@gmail.com>
2025-11-01 16:31:24 -0300

    feat: Adiciona constantes para categorias de frases
    Adiciona o objeto `PHRASE` dentro de `MotivationConstants` para centralizar as constantes que representam as categorias de frases.
    
    - `ALL` (0)
    - `HAPPY` (1)
    - `SUNNY` (2)

--- app/src/main/java/net/baudovalentim/motivation/utils/MotivationConstants.kt
+++ app/src/main/java/net/baudovalentim/motivation/utils/MotivationConstants.kt
@@ -4,4 +4,10 @@ class MotivationConstants private constructor(){
     object KEY {
         const val PERSON_NAME = "PERSON_NAME"
     }
+
+    object PHRASE {
+        const val ALL = 0
+        const val HAPPY = 1
+        const val SUNNY = 2
+    }
 }
