9577442d0a31db63d94eab563905509878bede5a
Fábio Valentim <maitabom@gmail.com>
2025-11-01 16:52:18 -0300

    feat: Implementa o filtro de categorias de frases
    Adiciona a funcionalidade de filtrar as frases por categoria na `MainActivity`. O usuário agora pode selecionar entre "Todas", "Feliz" e "Ensolarado" para ver frases correspondentes.
    
    - Implementa o método `handleFilter` para gerenciar a seleção da categoria, aplicando um filtro de cor branca ao ícone ativo e preto aos inativos.
    - Define a categoria "Todas" como padrão na inicialização da tela.
    - Atualiza o método `onClick` para identificar e tratar os cliques nos ícones de categoria.
    - Modifica a chamada a `phraseRepository.getPhrase()` para passar o filtro selecionado.
    - Adiciona `setOnClickListener` para os ícones de categoria (`image_all`, `image_happy`, `image_sunny`).
    - Altera a cor inicial do `tint` dos ícones de categoria no `activity_main.xml` de `white` para `black`.

--- app/src/main/java/net/baudovalentim/motivation/ui/MainActivity.kt
+++ app/src/main/java/net/baudovalentim/motivation/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import net.baudovalentim.motivation.R
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var securityPreferences: SecurityPreferences
     private val phraseRepository = PhraseRepository()
+    private var filter: Int = MotivationConstants.PHRASE.ALL
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,21 +37,51 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         setListeners()
         getUserName()
+        handleFilter(R.id.image_all)
         refreshPhrase()
     }
 
     override fun onClick(v: View?) {
+      val listId = listOf(
+          R.id.image_all,
+          R.id.image_happy,
+          R.id.image_sunny
+      )
+
       if (v?.id == R.id.button_new_phrase) {
-          handleNewPhrase()
+          refreshPhrase()
+      } else if (v?.id in listId) {
+          handleFilter(v?.id)
       }
     }
 
-    private fun refreshPhrase() {
-        binding.textPhrase.text = phraseRepository.getPhrase()
+    private fun handleFilter(id: Int?) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.black))
+
+        when (id) {
+            R.id.image_all -> {
+                filter = MotivationConstants.PHRASE.ALL
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+
+            R.id.image_happy -> {
+                filter = MotivationConstants.PHRASE.HAPPY
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+
+            R.id.image_sunny -> {
+                filter = MotivationConstants.PHRASE.SUNNY
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+        }
+
     }
 
-    private fun handleNewPhrase() {
-        TODO("Not yet implemented")
+    private fun refreshPhrase() {
+        binding.textPhrase.text = phraseRepository.getPhrase(filter)
     }
 
     private fun getUserName() {
@@ -58,7 +91,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun setListeners() {
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
     }
-
-
 }
